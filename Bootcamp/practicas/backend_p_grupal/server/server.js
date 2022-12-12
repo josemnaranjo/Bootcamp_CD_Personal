eb17a47cbeca9057f74f447e907b2fe1de9477e5
Josemaria Naranjo <josemnaranjoc@gmail.com>
2022-12-12 11:51:39 -0300

    commit 2

--- Bootcamp/practicas/backend_p_grupal/server/server.js
+++ Bootcamp/practicas/backend_p_grupal/server/server.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-require('./server/config/mongoose.config') 
+require('./config/mongoose.config') 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-require('./server/routes/routes')(app); 
+require('./routes/routes')(app);
 
  
 app.listen(8000, () => {
